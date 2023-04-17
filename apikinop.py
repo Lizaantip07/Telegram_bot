b0fed9de0a25e9d0232af9542bbd294a01b2bdb6
Lizaantip07 <118842531+Lizaantip07@users.noreply.github.com>
2023-04-17 17:46:21 +0200

    Create apikinop.py

---
+++ apikinop.py
@@ -0,0 +1,113 @@
+from random import choice
+
+import requests
+from typing import List
+
+
+def get_all_genres() -> List[str]:
+    with requests.Session() as session:
+        headers = {"X-API-KEY": "8efda388-c22d-4558-baa3-0f9cacac5bd9"}
+        with session.get("https://kinopoiskapiunofficial.tech/api/v2.2/films/filters", headers=headers) as response:
+            response_json = response.json()
+            answered_genres = []
+            for key, value in response_json.items():
+                if key == 'genres':
+                    for data in value:
+                        answered_genres.append(data['genre'])
+    return answered_genres
+
+
+def get_genres_by_ids(ids: List[int]) -> List[str]:
+    with requests.Session() as session:
+        headers = {"X-API-KEY": "8efda388-c22d-4558-baa3-0f9cacac5bd9"}
+        with session.get("https://kinopoiskapiunofficial.tech/api/v2.2/films/filters", headers=headers) as response:
+            response_json = response.json()
+            answered_genres = []
+            for key, value in response_json.items():
+                if key == 'genres':
+                    genre_by_id = {}
+                    for data in value:
+                        if data['id'] in ids:
+                            answered_genres.append(data['genre'])
+    return answered_genres
+
+
+def get_ids_by_genre(genres: List[str]) -> List[int]:
+    with requests.Session() as session:
+        headers = {"X-API-KEY": "8efda388-c22d-4558-baa3-0f9cacac5bd9"}
+        with session.get("https://kinopoiskapiunofficial.tech/api/v2.2/films/filters", headers=headers) as response:
+            response_json = response.json()
+            answered_ids = []
+            for key, value in response_json.items():
+                if key == 'genres':
+                    for data in value:
+                        if data['genre'] in genres:
+                            answered_ids.append(data['id'])
+    return answered_ids
+
+
+# print(get_genres_by_ids([1, 8, 3]))
+#
+# print(get_ids_by_genre(['триллер', 'комедия']))
+#
+# print(get_all_genres())
+
+
+def get_all_countries() -> List[str]:
+    with requests.Session() as session:
+        headers = {"X-API-KEY": "8efda388-c22d-4558-baa3-0f9cacac5bd9"}
+        with session.get("https://kinopoiskapiunofficial.tech/api/v2.2/films/filters", headers=headers) as response:
+            response_json = response.json()
+            answered_countries = []
+            for key, value in response_json.items():
+                if key == 'countries':
+                    for data in value:
+                        answered_countries.append(data['country'])
+    return answered_countries
+
+
+def get_countries_by_ids(ids: List[int]) -> List[str]:
+    with requests.Session() as session:
+        headers = {"X-API-KEY": "8efda388-c22d-4558-baa3-0f9cacac5bd9"}
+        with session.get("https://kinopoiskapiunofficial.tech/api/v2.2/films/filters", headers=headers) as response:
+            response_json = response.json()
+            answered_countries = []
+            for key, value in response_json.items():
+                if key == 'countries':
+                    for data in value:
+                        if data['id'] in ids:
+                            answered_countries.append(data['country'])
+    return answered_countries
+
+
+def get_ids_by_countries(countries: List[str]) -> List[int]:
+    with requests.Session() as session:
+        headers = {"X-API-KEY": "8efda388-c22d-4558-baa3-0f9cacac5bd9"}
+        with session.get("https://kinopoiskapiunofficial.tech/api/v2.2/films/filters", headers=headers) as response:
+            response_json = response.json()
+            answered_ids = []
+            for key, value in response_json.items():
+                if key == 'countries':
+                    for data in value:
+                        if data['country'] in countries:
+                            answered_ids.append(data['id'])
+    return answered_ids
+
+
+# print(get_all_countries())
+#
+# print(get_countries_by_ids([1, 13, 9]))
+#
+# print(get_ids_by_countries(['США', 'Канада', 'Германия']))
+
+
+def get_films_by_params(country_id: int, genre_id: int) -> dict:
+    with requests.Session() as session:
+        headers = {"X-API-KEY": "8efda388-c22d-4558-baa3-0f9cacac5bd9"}
+        with session.get(f"https://kinopoiskapiunofficial.tech/api/v2.2/films/?countries={country_id}&genres={genre_id}", headers=headers) as response:
+            random_film = choice(response.json()['items'])
+            return random_film
+
+
+print(get_genres_by_ids([2, 3, 7]))
+get_films_by_params(country_id=9, genre_id=5)
