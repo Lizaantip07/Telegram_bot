b52322d36596d948a2899888042c7e054d67a8ab
Lilia347 <118842509+Lilia347@users.noreply.github.com>
2023-04-18 20:33:07 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,230 +1,149 @@
 import logging
-from telegram.ext import Application, MessageHandler, filters, CommandHandler
-from telegram import ReplyKeyboardMarkup
-from TOKEN import BOT_TOKEN
 
-import datetime as dt
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
 
-reply_keyboard = [["/movie", "/series"]]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+from parsers import (
+    get_all_genres,
+    get_all_countries,
+    get_ids_by_genre,
+    get_ids_by_countries,
+    get_films_by_params,
+)
+from make_pretty_answ import pretty_answ_for_films
+from secrets import BOT_TOKEN
 
-GENRE_MESSAGES = {"/action": 'action', "/adventure": 'adventure', "/comedy": 'comedy',
-                  "/drama": 'drama', "/fantasy": 'fantasy', "/horror": 'horror',
-                  "/musicals": 'musicals', "/romance": 'romance', "/science_fiction": 'science_fiction',
-                  "/thriller": 'thriller'}
-
-COUNTRY_MESSAGES = {"/USA": 'USA', "/Russia": 'Russia', "/Canada": 'Canada',
-                    "/Spain": 'Spain', "/France": 'France', "/Italy": 'Italy',
-                    "/China": 'China', "/Japan": 'Japan'}
-
-CHOICE = {'genre': [], 'country': [], 'year': []}
+COUNTRY, YEAR, PREDICT = range(3)
 
 
-async def start(update, context):
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Привет {user.mention_html()}! Я - бот, который поможет Вам подобрать отличный фильм или сериал! Что хотите посмотреть?",
-        reply_markup=markup
+async def genre_choice(update, context) -> int:
+    reply_keyboard = [
+        [x, ]
+        for x in sorted(get_all_genres())
+    ]
+
+    await update.message.reply_text(
+        "Привет, я бот, который может предложить фильм тебе\n"
+        "Для этого тебе надо будет ответить на несколько вопросов, чтобы мой подбор был точнее \n"
+        "Отправь /cancel чтобы отменить выбор.\n"
+        "Итак, для начала, фильм какого жанра ты хочешь посмотреть:",
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard,
+            one_time_keyboard=True,
+            input_field_placeholder="genre choice",
+        ),
     )
 
-
-async def movie(update, context):
-    reply_keyboard = [
-        ["/yes"],
-        ["/no"],
-    ]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-    await update.message.reply_text("Вы хотите выбрать необходимые критерии?", reply_markup=markup)
+    return COUNTRY
 
 
-async def series(update, context):
-    reply_keyboard = [
-        ["/yes"],
-        ["/no"],
-    ]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-    await update.message.reply_text("Вы хотите выбрать необходимые критерии?", reply_markup=markup)
-
-
-async def no(update, context):
-    await update.message.reply_text("Хорошо, тогда я предоставлю Вам любой фильм.", reply_markup=markup)
-
-
-async def yes(update, context):
-    #real_chose = 'genre'
-    chat_id = update.message.text
-    reply_keyboard = [
-        ["/action"],
-        ["/adventure"],
-        ["/comedy"],
-        ["/drama"],
-        ["/fantasy"],
-        ["/horror"],
-        ["/musicals"],
-        ["/romance"],
-        ["/science_fiction"],
-        ["/thriller"],
-        ["/reset"],
-    ]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-    await update.message.reply_text("Выберите жанр:", reply_markup=markup)
-
-
-'''real_chose = 'genre'
-
-
-async def add_genre(update, context):
-    global real_chose
-    cnt = 0
-    reply_keyboard = [
-        ["/yes"],
-        ["/next_criterion"],
-    ]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-    message = update.message.text
-    is_genre = GENRE_MESSAGES.get(message)
-    if update.message.text == reply_keyboard[1][0] or real_chose == 'country':
-        real_chose = ''
-        await update.message.reply_text(f'Поехали дальше!')
-        await country(update, context)
-        cnt += 1
-    elif cnt == 1:
-        exit()
-    elif real_chose == 'genre':
-        CHOICE["genre"].append(is_genre)
-        await update.message.reply_text(f'Вы выбрали жанр {is_genre}. Хотите выбрать ещё один жанр или перейти на следующий критерий?', reply_markup=markup)
-        real_chose = 'genre'
-
-
-async def ask_add_genre(update, context):
-    chat_id = update.message.chat_id
-    await update.message.reply_text('Хотите выбрать еще жанр?', reply_markup=markup)
-    reply_keyboard = [["/да"], ["/нет"]] 
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-    await update.message.reply_text('Ок', reply_markup=markup)'''
-
-
-async def country(update, context):
-    #real_chose = 'country'
-    chat_id = update.message.chat_id
-    reply_keyboard = [
-        ["/USA"],
-        ["/Russia"],
-        ["/Canada"],
-        ["/Spain"],
-        ["/France"],
-        ["/Italy"],
-        ["/China"],
-        ["/Japan"],
-        ["/reset"],
-    ]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-    await update.message.reply_text("Выберите страну:", reply_markup=markup)
-
-
-'''async def add_country(update, context):
-    message = update.message.text
-    is_country = COUNTRY_MESSAGES.get(message)
-    await update.message.reply_text(f'Вы выбрали страну {is_country}.')
-    CHOICE["country"].append(is_country)'''
-
-
-'''async def ask_add_country(update, context):
-    chat_id = update.message.chat_id
-    await update.message.reply_text('Хотите выбрать еще страну?', reply_markup=markup)
-    reply_keyboard = [["/да"], ["/нет"]]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-    await update.message.reply_text('Ок', reply_markup=markup)'''
-
-
-async def year(update, context):
-    await update.message.reply_text("Введите год:", reply_markup=markup)
-
-
-async def reset(update, context):
-    chat_id = update.message.chat_id
-    reply_keyboard = [["/movie"], ["/series"]]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-    await update.message.reply_text('Вы вернулись в меню', reply_markup=markup)
-
-
-'''def Isdigit(message):
-    if 1927 <= int(message) <= 2022:
-        return True
-    else:
-        return False'''
-
-
-'''async def would_like_year(update, context):
-    reply_keyboard = [["/да"], ["/нет"]]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-    await update.message.reply_text('Хотите выбрать еще?', reply_markup=markup)
-    ms = update.message.text
-    if ms == '/да':
-        return True
-    else:
-        return False'''
-
-
-async def echo(update, context):
-    message = update.message.text
-    is_genre = GENRE_MESSAGES.get(message)
-    is_country = COUNTRY_MESSAGES.get(message)
-    is_year = 0
-    if CHOICE["genre"] == []:
-        CHOICE["genre"].append(is_genre)
-
-    if CHOICE["country"] == [None]:
-        CHOICE['country'] = []
-
-    if CHOICE["country"] == []:
-        CHOICE["country"].append(is_country)
-
-    if message.isdigit() and 1927 <= int(message) <= 2022:
-        is_year = message
-        if CHOICE["year"] == []:
-            CHOICE["year"].append(is_year)
-
-    if is_year:
-        await update.message.reply_text(f'Вы выбрали год {is_year}.')
-
-    if is_genre:
-        reply_keyboard = [
-            ["/country"],
+async def country_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    user = update.message.from_user
+    text = update.message.text
+    context.user_data["genre_choice"] = text
+    country_choices_keyboard = [
+        [
+            x,
         ]
-        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-        await update.message.reply_text(f'Вы выбрали жанр {is_genre}.', reply_markup=markup)
+        for x in get_all_countries()
+    ]
+    await update.message.reply_text(
+        "Отлично, идем дальше, теперь выбери страну производства \n"
+        "также можешь отправить команду /skip_country, чтобы пропустить этот параметр",
+        reply_markup=ReplyKeyboardMarkup(
+            country_choices_keyboard,
+            one_time_keyboard=True,
+            input_field_placeholder="genre choice",
+        ),
+    )
 
-    elif is_country:
-        reply_keyboard = [
-            ["/year"],
-        ]
-        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-        await update.message.reply_text(f'Вы выбрали страну {is_country}.', reply_markup=markup)
-
-    if CHOICE["genre"] != [] and CHOICE["country"] != [] and CHOICE["year"] != []:
-        print(CHOICE)
+    return YEAR
 
 
-def main():
-    application = Application.builder().token(BOT_TOKEN).build()
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("movie", movie))
-    application.add_handler(CommandHandler("series", series))
-    application.add_handler(CommandHandler("no", no))
-    application.add_handler(CommandHandler("yes", yes))
-    #application.add_handler(MessageHandler(filters.TEXT, add_genre))
-    application.add_handler(CommandHandler("country", country))
-    #application.add_handler(MessageHandler(filters.TEXT, add_country))
-    application.add_handler(CommandHandler("year", year))
-    application.add_handler(CommandHandler("reset", reset))
-    application.add_handler(MessageHandler(filters.TEXT, echo))
+async def year_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    user = update.message.from_user
+    text = update.message.text
+    context.user_data["country_choice"] = text
+    await update.message.reply_text(
+        "Теперь отправь целое число - минимальный год, старше которого должен быть фильм\n",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+    return PREDICT
+
+
+async def get_predict(update, context):
+    user = update.message.from_user
+    year_text = update.message.text
+    try:
+        if int(year_text) not in range(1910, 2022):
+            await update.message.reply_text(
+                "Ты нам назвал плохое число, мы его учитывать не будем",
+            )
+    except ValueError as e:
+        await update.message.reply_text(
+            "Ты нам назвал вообще не число, мы его учитывать не будем",
+        )
+    context.user_data["year_choice"] = year_text
+    params = list()
+    params.append(("genres", get_ids_by_genre(context.user_data["genre_choice"])))
+    if context.user_data["country_choice"]:
+        params.append(
+            ("countries", get_ids_by_countries(context.user_data["country_choice"]))
+        )
+    if context.user_data["year_choice"]:
+        params.append(
+            ("yearFrom", get_ids_by_countries(context.user_data["country_choice"]))
+        )
+    response = get_films_by_params(params)
+    if not response:
+        await update.message.reply_text(
+            "Фильма с такими параметрами на нашлось, попробуй другой набор фильтров"
+        )
+        return ConversationHandler.END
+    else:
+        res = "\n".join(pretty_answ_for_films(response))
+        await update.message.reply_text(res)
+        return ConversationHandler.END
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    user = update.message.from_user
+    await update.message.reply_text(
+        "Пока, надеюсь вернешься к нам снова", reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
+
+
+def main() -> None:
+    application = (
+        Application.builder()
+        .token(BOT_TOKEN)
+        .build()
+    )
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("film", genre_choice)],
+        states={
+            COUNTRY: [
+                MessageHandler(filters.Text(get_all_genres()), country_choice),
+            ],
+            YEAR: [MessageHandler(filters.Text(get_all_countries()), year_choice)],
+            PREDICT: [MessageHandler(filters.Text(), get_predict)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
+
+    application.add_handler(conv_handler)
     application.run_polling()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
-
-
-
